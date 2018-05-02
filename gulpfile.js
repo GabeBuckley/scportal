28a40b6cfae2dd1ed59233f48ff41d1e46e147b8
Gabe Buckley <Gabe.Buckley@dominos.com.au>
2018-05-02 16:05:05 +1000

    Build Gulp Pipe to split index.html into multiple cshtml layout parts

--- gulpfile.js
+++ gulpfile.js
@@ -2,83 +2,55 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const clean = require('gulp-clean');
 const replace = require('gulp-replace');
-const extract_text = require('gulp-extract-text');
+const htmlsplit = require('gulp-htmlsplit');
 
 var app = {
-	cshtmlDir: './cshtml',
-	cshtmlFiles: [
-		{
-			filename: '_head.cshtml',
-			startTag: '<!-- START HEAD BLOCK -->',
-			endTag: '<!-- END HEAD BLOCK -->'
-		},
-		{
-			filename: '_pageHead.cshtml',
-			startTag: '<!-- START PAGE_HEADER BLOCK -->',
-			endTag: '<!-- END PAGE_HEADER BLOCK -->'
-		},
-		{
-			filename: '_pageHead.cshtml',
-			startTag: '<!-- START PAGE_HEADER BLOCK -->',
-			endTag: '<!-- END PAGE_HEADER BLOCK -->'
-		},
-		{
-			filename: '_pageMainTop.cshtml',
-			startTag: '<!-- START PAGE_MAIN_TOP BLOCK -->',
-			endTag: '<!-- END PAGE_MAIN_TOP BLOCK -->'
-		},
-		{
-			filename: '_pages.cshtml',
-			startTag: '<!-- START PAGES BLOCK -->',
-			endTag: '<!-- END PAGES BLOCK -->'
-		},
-		{
-			filename: '_pages.cshtml',
-			startTag: '<!-- START PAGES BLOCK -->',
-			endTag: '<!-- END PAGES BLOCK -->'
-		},
-		{
-			filename: '_pageMainBot.cshtml',
-			startTag: '<!-- START PAGE_MAIN_BOT BLOCK -->',
-			endTag: '<!-- END PAGE_MAIN_BOT BLOCK -->'
-		},
-		{
-			filename: '_pageDlg.cshtml',
-			startTag: '<!-- START PAGE_DLG BLOCK -->',
-			endTag: '<!-- END PAGE_DLG BLOCK -->'
-		}
-	]
-	]
+	cshtmlDir: './assets/cshtml',
+	headFile: '_head.cshtml',
+	scriptTags: ''
 }
 
 
+gulp.task('generateCSHTML', ['clean'], function () {
+	return gulp.src('./index.html')
+		.pipe(htmlsplit())
+		.pipe(gulp.dest(app.cshtmlDir));
+});
 
-let extract_text_params = {
-	pattern_start: "<head>",
-	pattern_end: "</head>"
-}
 
-gulp.task('generateCSHTML', function () {
-	for (let i = 0; i < app.cshtmlFiles.length; i++) {
-		var _file = app.cshtmlFiles[i];
-		var _param = {
-			pattern_start: _file.startTag,
-			pattern_end: _file.endTag
-		}
+gulp.task('replaceTags', ['generateCSHTML'], function () {
+	var reStyleStart = /<link rel=\"stylesheet\" href=\"/gi;
+	var reStyleEnd = /\.css\"\s*\/>/gi;
+	var reScriptStart = /<script src=\"\./gi;
+	var reScriptEnd = /\.js\"\s*><\/script>/gi;
 
-	}
-	gulp.src(['index.html'])
-		.pipe(extract_text(_param))
-		.pipe(gulp.dest(app.cshtmlDir + '/' + _file.filename));
+	var csStyleStart = '@Styles.Render("~';
+	var csStyleEnd = '.css")';
+	var csScriptStart = '@Scrupts.Render("~';
+	var csScriptEnd = '.js")';
+
+
+	return gulp.src([app.cshtmlDir + '/' + app.headFile])
+		.pipe(replace(reStyleStart, csStyleStart))
+		.pipe(replace(reStyleEnd, csStyleEnd))
+		.pipe(replace(reScriptStart, csScriptStart))
+		.pipe(replace(reScriptEnd, csScriptEnd))
+		.pipe(gulp.dest(app.cshtmlDir));
 });
 
 
 gulp.task('clean', function () {
-	return gulp.src(app.cshtmlDir)
+	return gulp.src(app.cshtmlDir + '/*.*')
 		.pipe(clean());
 });
 
 
-gulp.task('default', ['clean'], function () {
-	gulp.start(['generateCSHTML']);
+gulp.task('default', ['replaceTags'], function () {
+	console.log('Done');
 });
+
+/**
+gulp.task('default', ['replaceFags'], function () {
+	console.log('Done');
+});
+**/
