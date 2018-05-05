e7f8b9ec7ffa7049af243f50e3fbf5e21773d6c5
Gabe Buckley <Gabe.Buckley@dominos.com.au>
2018-05-05 12:37:52 +1000

    Improvements to build pipeline. Rebases index.html into separate files, rebases JS and CSS files, productes distribution version in site subfolder

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,33 @@ const htmlsplit = require('gulp-htmlsplit');
 
 var app = {
 	cshtmlDir: './assets/cshtml',
+	tempDir: './tmp',
 	headFile: '_head.cshtml',
+	indexFile: 'index.html',
 	scriptTags: ''
 }
 
-
-gulp.task('generateCSHTML', ['clean'], function () {
-	return gulp.src('./index.html')
-		.pipe(htmlsplit())
-		.pipe(gulp.dest(app.cshtmlDir));
+gulp.task('default', ['tidy'], function () {
+	console.log('Done');
 });
 
+gulp.task('tidy', ['replaceTags'], function () {
+	// Clean out the Temp Directory
+	return gulp.src(app.tempDir + '/*.*')
+		.pipe(clean());
+});
 
 gulp.task('replaceTags', ['generateCSHTML'], function () {
-	var reStyleStart = /<link rel=\"stylesheet\" href=\"/gi;
+	// Replace script and style tags with C# @ declarations
+	var reStyleStart = /<link rel=\"stylesheet\" href=\"\.*/gi;
 	var reStyleEnd = /\.css\"\s*\/>/gi;
-	var reScriptStart = /<script src=\"\./gi;
+	var reScriptStart = /<script src=\"\.*/gi;
 	var reScriptEnd = /\.js\"\s*><\/script>/gi;
 
 	var csStyleStart = '@Styles.Render("~';
 	var csStyleEnd = '.css")';
 	var csScriptStart = '@Scrupts.Render("~';
 	var csScriptEnd = '.js")';
-
 
 	return gulp.src([app.cshtmlDir + '/' + app.headFile])
 		.pipe(replace(reStyleStart, csStyleStart))
@@ -38,19 +42,22 @@ gulp.task('replaceTags', ['generateCSHTML'], function () {
 		.pipe(gulp.dest(app.cshtmlDir));
 });
 
+gulp.task('generateCSHTML', ['rebase'], function () {
+	return gulp.src(app.tempDir + '/' + app.indexFile)
+		.pipe(htmlsplit())
+		.pipe(gulp.dest(app.cshtmlDir));
+});
+
+gulp.task('rebase', ['clean'], function () {
+	var reSiteBase = /\/assets\//gi;
+	var csSiteBase = '/site/assets/';
+
+	return gulp.src([app.indexFile])
+		.pipe(replace(reSiteBase, csSiteBase))
+		.pipe(gulp.dest(app.tempDir));
+});
 
 gulp.task('clean', function () {
 	return gulp.src(app.cshtmlDir + '/*.*')
 		.pipe(clean());
 });
-
-
-gulp.task('default', ['replaceTags'], function () {
-	console.log('Done');
-});
-
-/**
-gulp.task('default', ['replaceFags'], function () {
-	console.log('Done');
-});
-**/
